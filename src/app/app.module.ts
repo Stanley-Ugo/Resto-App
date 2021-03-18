20b856ce3052de48a81059d5194be99cf3bf1ed7
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-03-18 01:22:32 +0100

    Adding a add-resto component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddRestoComponent } from './add-resto/add-resto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddRestoComponent
   ],
   imports: [
     BrowserModule,
