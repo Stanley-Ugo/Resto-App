e96653cb9824121dc09bfa9c15b1b6f5ab3e268d
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-03-18 01:23:39 +0100

    Adding a update-resto component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddRestoComponent } from './add-resto/add-resto.component';
+import { UpdateRestoComponent } from './update-resto/update-resto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddRestoComponent
+    AddRestoComponent,
+    UpdateRestoComponent
   ],
   imports: [
     BrowserModule,
