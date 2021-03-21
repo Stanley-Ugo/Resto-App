b5f87ef1c599142f8968c7d23132cf3a3f8e0284
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-03-21 12:12:43 +0100

    Importing & injecting the service in list-resto

--- src/app/list-resto/list-resto.component.ts
+++ src/app/list-resto/list-resto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestoService } from '../resto.service';
 
 @Component({
   selector: 'app-list-resto',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListRestoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private resto:RestoService) { }
 
   ngOnInit(): void {
   }
