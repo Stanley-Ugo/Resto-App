b0e228908f8b1abc36ac77f31599b62cc91faa6d
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-03-24 22:42:38 +0100

    Adding the Register User file

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  alert:boolean = false;
+  register = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl('')
+  })
   constructor() { }
 
   ngOnInit(): void {
